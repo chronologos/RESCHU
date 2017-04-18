89be93c5efbb2202f77488fdc9d785aabb5f18c2
hz99@duke.edu <hz99@duke.edu>
2017-04-17 20:50:39 -0400

    fix compact panel bug of showing only 3 UAVs

--- RESCHU_Eclipse/src/reschu/game/view/PanelControl.java
+++ RESCHU_Eclipse/src/reschu/game/view/PanelControl.java
@@ -175,7 +175,7 @@ class VehicleGeneralPanel extends JPanel {
 	public VehicleGeneralPanel(GUI_Listener l, Game g) {
 		double size_1[][] = {{TableLayout.FILL},{51,51,51}};
 		double size_2[][] = {{TableLayout.FILL},{51,51,51,51,51,51}};
-		if(g.getScenario()==1) setLayout(new TableLayout(size_1));
+		if(g.getScenario()==0) setLayout(new TableLayout(size_1));
 		else setLayout(new TableLayout(size_2));
 		game = g; 
 		infoList = new VehicleCompactInfo[game.getVehicleList().size()];
